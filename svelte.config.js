f7a98253e521002d6700065b8da9d43df683eb90
Harrison <novox187@gmail.com>
2026-04-30 20:53:30 +0300

    Mejora: Cambiar al adaptador de Node.js para la implementación en producción
    Reemplaza el adaptador automático por el adaptador de Node.js para tener control total sobre el entorno del servidor. Este cambio permite la implementación en plataformas de alojamiento de Node.js y proporciona un servidor Node.js funcional para su uso en producción.
    
    Añade las dependencias necesarias y actualiza la configuración de SvelteKit según corresponda. Además, reordena `@types/node` para mantener un orden de dependencias coherente.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node'; // Cambia adapter-auto por adapter-node
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter() // Esto generará un servidor Node.js funcional
 	}
 };
 
